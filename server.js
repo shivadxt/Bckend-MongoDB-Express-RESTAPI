61cb507138cefb0943343af355c4b17cd5473954
shivadxt <shivamweb3.0@gmail.com>
2024-06-08 17:25:32 +0530

    4th commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-mongoose.connect('mongodb://localhost/subscribers');
+mongoose.connect(process.env.MONGO_URL);
 const db = mongoose.connection
 db.on('error',(error)=> console.error(error));
 db.once('open',()=> console.log("Connected to Database:"));
