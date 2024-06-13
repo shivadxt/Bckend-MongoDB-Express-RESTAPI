5e1f7fd64089d5d436e31fd7120c4be7a1333008
shivadxt <shivamweb3.0@gmail.com>
2024-06-13 18:54:11 +0530

    All routes added

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const Subscriber = require('../models/subscriber');
+const mongoose = require('mongoose');
+
 
 const router = express.Router();
 
@@ -8,7 +10,7 @@ router.get('/', async (req, res)=>{
         const subscribers = await Subscriber.find()
         res.json(subscribers);
     } catch (error) {
-        res.status(500).json({ message: err.message})
+        res.status(500).json({ message: error.message})
     }
 });
 
@@ -23,20 +25,68 @@ router.post('/', async(req, res) => {
         const newSubscriber = await subscriber.save()
         res.status(201).json(newSubscriber);
     } catch (error) {
-        res.status(400).json({ message: err.message});
+        res.status(400).json({ message: error.message});
     }
 })
 
-router.post('/:id',(req, res) => {
-    
+//Getting one user
+router.get('/:id',validateObjectId, getSubscriber, async (req, res)=>{
+    // console.log(subscriber)
+    res.json({id:res.subscriber._id , name:res.subscriber.name})
+    //Instead of sending just the name field, you can construct an object that includes 
+    // both the name and id fields and then send that object as the JSON response.
+});
+
+
+//Update single user
+router.patch('/:id',validateObjectId, getSubscriber, async (req,res) => {
+    if(req.body.name != null){ 
+        res.subscriber.name = req.body.name
+    }
+    if(req.body.channelSubscribedTo != null){
+        res.subscriber.channelSubscribedTo = req.body.channelSubscribedTo
+    }
+    try {
+        const updatedSubscriber = await res.subscriber.save()
+        res.json(updatedSubscriber)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
 })
 
-router.patch('/:id',(req,res) => {
-    
+//Delete single user
+router.delete('/:id',validateObjectId, getSubscriber, async(req,res) => {
+    try {
+        await res.subscriber.deleteOne();
+        res.json({ message: 'Deleted Subscriber' });
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
 })
 
-router.delete('/:id', async(req,res) => {
+//Middleware- to get elements by ID which can be used in other routes
 
-})
+async function getSubscriber (req, res, next) {
+    let subscriber
+    try {
+        subscriber = await Subscriber.findById(req.params.id)
+        // if (subscriber == null) {
+        //     return res.status(404).json({ message : 'Cannot find subscriber' })
+        // }
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
+
+    res.subscriber = subscriber;
+    next()
+}
+
+// Middleware to validate ObjectId
+function validateObjectId(req, res, next) {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(404).json({ message: `Cann't Find Subscriber !` });
+    }
+    next();
+}
 
 module.exports = router;
